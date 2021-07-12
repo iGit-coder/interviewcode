8f16011fd564b4b4ae7b907f6f444ed6b13e17b7
iGit-coder <13700301072@163.com>
2021-07-12 19:36:26 +0800

    app修改

--- src/App.java
+++ src/App.java
@@ -5,6 +5,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         TestInterface ti = new SortArgorithm();
-        ti.test();
+        ti.test();//
     }
 }
